bbd961d40e683902cfa8fc0cc231462c6d00ca5c
unknown <coolbluealan@gmail.com>
2018-07-15 13:09:16 -0400

    done project 28

--- Project Euler 28.js
+++ Project Euler 28.js
@@ -1,2 +1,14 @@
-var sum;
-var addSum;
+var sum=1;
+var add=3;
+var ad2=2;
+var counter=0;
+while(add<1001*1001+1){
+    sum+=add;
+    counter++;
+    if(counter%4===0){
+        ad2+=2;
+        counter=0;
+    }
+    add+=ad2;
+}
+console.log(sum);
