771794bdcc97c9b3c67ad843e8c2cf5459615ef9
coolbluealan <39867758+coolbluealan@users.noreply.github.com>
2018-07-19 21:07:57 -0400

    the working prime sieve

--- Other/Prime numbers.js
+++ Other/Prime numbers.js
@@ -1,17 +1,19 @@
 //use sieve algorithm
-var flag=Array(11);
-flag.fill(1);
-flag[0]=0;
-flag[1]=0;
-var primes=[]
-for(var i=2;i<flag.length;i++){
-    if(flag[i]===1){
-        primes.push(i);
-        counter=i;
-        while(counter<=flag.length){
-            counter+=i;
-            flag[counter]=0;
+var sieve=function(n){
+    var flag=Array(n);
+    flag.fill(1);
+    flag[0]=0;
+    flag[1]=0;
+    var primes=[];
+    for(var i=2;i<n;i++){
+        if(flag[i]===1){
+            primes.push(i);
+            counter=i;
+            while(counter<n){
+                counter+=i;
+                flag[counter]=0;
+            }
         }
     }
+    return primes;
 }
-console.log(primes.join());
